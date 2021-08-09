1bf7e8bbb008b5364d1cc70e64949d812e140207
shinji_sato <shinji_sato_7@hotmail.com>
2021-08-09 01:13:32 -0300

    update js

--- Amulets.js
+++ Amulets.js
@@ -14,9 +14,9 @@ function amountDarumaRed(e) {
   let amount = e.target;
   console.log(amount.children[2].value);
   alert(nombre + " " + apellido + "\n¡Has agregado " + amount.children[2].value + " unidades al carrito!");
-}
 
-const amount = document.getElementById("amount").value;
+  console.log(red.tipo + red.color + "Pedido: " + amount.children[2].value + " Precio: " + red.precio);
+}
 
 let cartDarumaBlue = document.getElementById("cart_daruma-blue");
 cartDarumaBlue.addEventListener("submit", amountDarumaBlue);
@@ -48,23 +48,22 @@ function amountDarumaGreen(e) {
   alert(nombre + " " + apellido + "\n¡Has agregado " + amount.children[2].value + " unidades al carrito!");
 }
 
-class Amuletos {
-  constructor(tipo, color, tamaño, precio, stock) {
+class Products {
+  constructor(tipo, color, precio, stock) {
     this.tipo = tipo;
     this.color = color;
-    this.tamaño = tamaño;
     this.precio = precio;
     this.stock = stock;
   }
 }
+const red = new Products("Daruma: ", "Rojo ", 250, " stock: 15");
+const green = new Products("Daruma: ", "Verde ", 260, " stock: 10");
+const blue = new Products("Daruma: ", "Azul ", 270, " stock: 8");
+const black = new Products("Daruma: ", "Negro ", 280, " stock: 11 ");
 
-const darumaRojo = new Amuletos("Daruma: ", "Rojo ", "Mediano ", 200, " stock: 15");
-const darumaVerde = new Amuletos("Daruma: ", "Verde ", "Mediano ", 200, " stock: 10");
-const darumaAzul = new Amuletos("Daruma: ", "Azul ", "Mediano ", 200, " stock: 8");
-
-const omamoriSuerte = new Amuletos("Omamori: ", "Suerte ", "Pequeño ", 150, " stock: 20");
-const omamoriSalud = new Amuletos("Omamori: ", "Salud ", "Pequeño ", 150, " stock: 17");
-const omamoriAmor = new Amuletos("Omamori: ", "Amor ", "Pequeño ", 150, " stock: 13");
+const lucky = new Products("Omamori: ", "Suerte ", 150, " stock: 20");
+const health = new Products("Omamori: ", "Salud ", 150, " stock: 17");
+const love = new Products("Omamori: ", "Amor ", 150, " stock: 13");
 
 // const pedidoProducto = [];
 // const pedidoDaruma = [];
